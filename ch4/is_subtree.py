7ac495d75cbaf0116e9742b17fdd216eac6e4372
ekougs <ekougs@gmail.com>
2016-04-20 16:59:34 +0000

    Ex 4.7: init

---
+++ ch4/is_subtree.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+
+
+class IsSubtreeTest(TestCase):
+    def test_should_be_subtree(self):
+        pass
+
+    def test_should_not_be_subtree(self):
+        pass
---
+++ ch4/is_subtree_test.py
@@ -0,0 +1,12 @@
+"""
+Implementation relative to ex 4.7 about big trees and subtrees
+"""
+
+
+def is_subtree(tree, subtree):
+    """
+    :param tree: the containing tree
+    :param subtree: the contained tree
+    :return: true if subtree is contained by tree false otherwise
+    """
+    pass
